2dda7553c0a26e30d906aed1ddb24f2c6573a000
Sachin Gurjar <sachinhlo232@gmail.com>
2024-06-30 17:27:08 +0530

    home page starts

--- client/src/app/(home)/home/page.tsx
+++ client/src/app/(home)/home/page.tsx
@@ -1,15 +1,27 @@
+import { Heading } from "@/components/ui/typography";
 import { getServerAuth } from "@/lib/auth";
+import { getGreeting } from "@/lib/utils";
+import { format } from "date-fns";
 import React from "react";
 
 export default async function Page() {
   const session = await getServerAuth();
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome {session.user.name}</p>
-      your workspace is {session.user.workspace}
-      invitation token is: {session?.invitationToken || ""}
-
+    <div className="mt-8 px-6">
+      {/* <h1>Home</h1> */}
+      {/* <p>Welcome {session.user.name}</p> */}
+      {/* your workspace is {session.user.workspace} */}
+      <div className="space-y-2 my-4">
+        <Heading className="font-semibold">Home</Heading>
+      </div>
+      <div className="flex flex-col gap-y-1 items-center w-full">
+        <Heading variant="h5" className="font-normal">
+          {format(new Date(), "EEEE, MMMM d")}
+        </Heading>
+        <Heading variant="h2" className="font-normal">
+          {getGreeting()}, {session.user.name}
+        </Heading>
+      </div>
     </div>
   );
 }
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -60,3 +60,20 @@ export const getColorForName = (name: string): string => {
   const hash = stringToHash(name);
   return hashToColor(hash);
 };
+
+
+
+export const getGreeting = () => {
+  const date = new Date();
+  const hour = date.getHours();
+
+  if (hour < 12) {
+    return "Good morning";
+  } else if (hour < 18) {
+    return "Good afternoon";
+  } else if (hour < 21) {
+    return "Good evening";
+  } else {
+    return "Good night";
+  }
+};
