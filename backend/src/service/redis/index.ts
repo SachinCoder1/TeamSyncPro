e3eb589b57da8b1a3fab96d59a51b074a3297357
Sachin Gurjar <sachinhlo232@gmail.com>
2024-08-28 20:15:23 +0530

    redis docker file

--- backend/src/service/redis/index.ts
+++ backend/src/service/redis/index.ts
@@ -1,5 +1,8 @@
 import { Redis } from 'ioredis';
-const redisClient = new Redis(); // default connects to 127.0.0.1:6379
+const redisClient = new Redis({
+    host: 'redis',
+    port: 6379,  
+}); // default connects to 127.0.0.1:6379
 
 
 export default redisClient;
