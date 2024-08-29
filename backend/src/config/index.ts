4c11cf8f1548382b3c7a98f628db5132203b3736
Sachin Gurjar <sachinhlo232@gmail.com>
2024-08-29 15:44:10 +0530

    redis global config, redis seetup

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -7,20 +7,34 @@ const config = {
   [DEVELOPMENT]: {
     BASE_URL: "http://localhost:8080",
     CLIENT_URL: "http://localhost:3000",
+    REDIS: {
+      HOST: "127.0.0.1",
+      PORT: 6379
+    }
   },
   [TESTING]: {
     BASE_URL: "",
     CLIENT_URL: "",
+    REDIS: {
+      HOST: "127.0.0.1",
+      PORT: 6379
+    }
   },
   [PRODUCTION]: {
     BASE_URL: "https://teamsynpro-backend.sachingurjar.me",
     CLIENT_URL: "https://teamsyncpro.sachingurjar.me",
+    REDIS: {
+      HOST: "redis",
+      PORT: 6379
+    }
+
   },
 };
 
 const envConfig = config[ENV];
 export const BASE_URL = envConfig.BASE_URL;
 export const CLIENT_URL = envConfig.CLIENT_URL;
+export const REDIS_URL = envConfig.REDIS;
 
 export const API_VERSION = "v1";
 
--- backend/src/service/redis/index.ts
+++ backend/src/service/redis/index.ts
@@ -1,6 +1,10 @@
-import { Redis } from 'ioredis';
-const redisClient = new Redis(); // default connects to 127.0.0.1:6379
+import { Redis } from "ioredis";
+import { REDIS_URL } from "~/config";
 
+const redisClient = new Redis({
+  host: REDIS_URL.HOST,
+  port: REDIS_URL.PORT,
+}); // default connects to 127.0.0.1:6379
 
 export default redisClient;
-export * from './redisHelper'
+export * from "./redisHelper";
